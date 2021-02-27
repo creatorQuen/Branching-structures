d11781d2e632875614fa58ae832ae9b85dd3c4f1
machine.steam@yandex.ru <machine.steam@yandex.ru>
2021-02-27 22:00:20 +0300

    Добавьте файлы проекта.

---
+++ A_Task/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace A_Task
+{
+    /// <summary>
+    /// Пользователь вводит 2 числа (A и B). 
+    /// Если A>B, подсчитать A+B, если A=B, подсчитать A*B, если A<B, подсчитать A-B.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstInput;
+            string secondInput;
+
+            Console.WriteLine("Введиде два числа");
+
+            Console.WriteLine("Первое число (A):");
+            firstInput = Console.ReadLine();
+            double  firstNumber = Convert.ToDouble(firstInput);
+
+            Console.WriteLine("Второе число (B):");
+            secondInput = Console.ReadLine();
+            double secondNumber = Convert.ToDouble(secondInput);
+
+            if (firstNumber > secondNumber)
+            {
+                var operattionPlus = firstNumber + secondNumber;
+                Console.WriteLine($"A+B равно:{operattionPlus}");
+            }
+            else if (firstNumber == secondNumber)
+            {
+                Console.WriteLine($"A*B равно: {firstNumber * secondNumber}");
+            }
+            else if (firstNumber < secondNumber)
+            {
+                Console.WriteLine($"A-B равно:{firstNumber - secondNumber}");
+            }
+
+        }
+    }
+}
